9661ad9f0c433da570cff01ccbce2d8ce74e9b5f
Noel Lopes <noel@ipg.pt>
2026-09-10 15:51:18 +0100

    feat: export parsed request value types

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ mod web_page_resource_handler;
 pub use content_provider::ContentProvider;
 pub use custom_resource_handler::CustomResourceHandlerFactory;
 pub use local_file_resource_handler::LocalFileResourceHandlerFactory;
-pub use request_info::RequestInfo;
+pub use request_info::{ParsedRequestValues, PostDataValues, QueryParameters, RequestInfo};
 pub use resource_handler_error::ResourceHandlerError;
 pub use web_page_handler::WebPageHandler;
 pub use web_page_resource_handler::WebPageResourceHandlerFactory;
